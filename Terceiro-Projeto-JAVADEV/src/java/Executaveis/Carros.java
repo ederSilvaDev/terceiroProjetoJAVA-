a799035563ff56482b584b7de537adf4e691ed92
ederSilvaDev <81658662+ederSilvaDev@users.noreply.github.com>
2021-04-12 10:53:25 -0300

    Construindo a Classe Carros

---
+++ Terceiro-Projeto-JAVADEV/src/java/Executaveis/Carros.java
@@ -0,0 +1,42 @@
+package java.Executaveis;
+
+public class Carros {
+	
+	private String modelo;
+	private String marca;
+	private String tipo;
+	
+	public String getModelo() {
+		return modelo;
+	}
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public String getCor() {
+		return cor;
+	}
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+	public String getMotor() {
+		return motor;
+	}
+	public void setMotor(String motor) {
+		this.motor = motor;
+	}
+	private String cor;
+	private String motor;
+
+}
