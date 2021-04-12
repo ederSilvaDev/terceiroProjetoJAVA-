0258f8c6061d28c1c8c20381c682b09094fe0094
ederSilvaDev <81658662+ederSilvaDev@users.noreply.github.com>
2021-04-12 11:23:27 -0300

    Implementação de METODOS

--- Terceiro-Projeto-JAVADEV/src/java/Executaveis/Carros.java
+++ Terceiro-Projeto-JAVADEV/src/java/Executaveis/Carros.java
@@ -38,5 +38,32 @@ public class Carros {
 	public void setMotor(String motor) {
 		this.motor = motor;
 	}
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return super.hashCode();
+	}
+	@Override
+	public boolean equals(Object obj) {
+		// TODO Auto-generated method stub
+		return super.equals(obj);
+	}
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		return super.clone();
+	}
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return super.toString();
+	}
+	@SuppressWarnings("deprecation")
+	@Override
+	protected void finalize() throws Throwable {
+		// TODO Auto-generated method stub
+		super.finalize();
+	}
+		
 
 }
